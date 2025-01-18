beb01713e172e1487956406d29c4b897ad3ae56b
Mohammadfaiz Bawa <mbawa@opentext.com>
2025-01-18 23:53:19 +0530

    Initial commit

---
+++ gui.py
@@ -0,0 +1,10 @@
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a To-do")
+input_box = sg.InputText(tooltip="Enter To-do")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-do App', layout=[[label], [input_box], [add_button]])
+
+window.read()
+window.close()
